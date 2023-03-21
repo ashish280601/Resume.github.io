84aeee3ccc7cb7ae0737099a6838f5abb4af434b
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-03-21 12:04:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// smooth scroll
+
 var newMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 console.log(newMenuAnchorTags);
 for (i = 0 ; i < newMenuAnchorTags.length; i++){
@@ -17,4 +19,46 @@ for (i = 0 ; i < newMenuAnchorTags.length; i++){
             },20);
     });
 }
+//create an skills bar 
+var progressBars = document.querySelectorAll('.skill-progress > div');
+var skillsContainer = document.getElementById('skills-container');
+window.addEventListener('scroll', checkScroll);
+var animationDone = false;
 
+function initialiseBars(){
+    for (let bar of progressBars){
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+function fillBars(){
+
+    for (let bar of progressBars){
+        let targetWidth = bar.getAttribute('data-bar-width');
+        let currentWidth = 0;
+        let interval = setInterval(function(){
+            if(currentWidth > targetWidth){
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        },3);
+    }
+}
+
+function checkScroll(){
+
+    // have to check wheather skills container is visible
+    var coordinates  = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top < window.innerHeight){
+        animationDone = true;
+        console.log('Skills Section is Visible');
+        fillBars();
+    }else if(coordinates.top > window.innerHeight){
+        animationDone = false;
+        initialiseBars();
+    }
+}
